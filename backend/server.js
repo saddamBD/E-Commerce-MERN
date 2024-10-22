956b281c7323db6f07f2c0474949079257f75e55
saddamBD <saddam.ruet@gmail.com>
2024-10-22 14:47:05 +0600

    cart route

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import cartRoutes from "./routes/cart.route.js";
 
 const app = express();
 
@@ -15,6 +16,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("api/products", productRoutes);
+app.use("api/cart", cartRoutes);
 
 app.listen(PORT, () => {
   console.log("App is listening on http://localhost:" + PORT);
