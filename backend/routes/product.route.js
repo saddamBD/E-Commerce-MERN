c3545f9f56c5d4b3dae89eda7dea614890acf33c
saddamBD <saddam.ruet@gmail.com>
2024-10-22 13:47:23 +0600

    deleteProduct route

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -2,6 +2,7 @@ import express from "express";
 import { adminRoute, protectRoute } from "../middleware/auth.middleware";
 import {
   createProduct,
+  deleteProduct,
   getAllProducts,
   getFeaturedProducts,
 } from "../controllers/product.controller";
@@ -11,5 +12,6 @@ const router = express.Router();
 router.get("/", protectRoute, adminRoute, getAllProducts);
 router.get("/featured", getFeaturedProducts);
 router.post("/", protectRoute, adminRoute, createProduct);
+router.delete("/:id", protectRoute, adminRoute, deleteProduct);
 
 export default router;
