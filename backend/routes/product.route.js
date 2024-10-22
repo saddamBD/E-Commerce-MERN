a7e5d34b08a18b2ae0a48a7e351c2a3d587b8eed
saddamBD <saddam.ruet@gmail.com>
2024-10-22 12:14:51 +0600

    featuredProduct

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,8 +1,13 @@
 import express from "express";
 import { adminRoute, protectRoute } from "../middleware/auth.middleware";
+import {
+  getAllProducts,
+  getFeaturedProducts,
+} from "../controllers/product.controller";
 
 const router = express.Router();
 
 router.get("/", protectRoute, adminRoute, getAllProducts);
+router.get("/featured", getFeaturedProducts);
 
 export default router;
