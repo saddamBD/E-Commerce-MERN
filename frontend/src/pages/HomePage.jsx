9127597903bc951ace5ce3c092bb2bc8bb5b00d2
saddamBD <saddam.ruet@gmail.com>
2024-10-23 20:21:04 +0600

    recommended products

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,4 +1,8 @@
+
+import { useEffect } from "react";
 import CategoryItem from "../components/CategoryItem";
+import { useProductStore } from "../stores/useProductStore";
+import FeaturedProducts from "../components/FeaturedProducts";
 
 
 const categories = [
@@ -12,6 +16,12 @@ const categories = [
 ];
 
 const HomePage = () => {
+
+  const {fetchFeaturedProducts,products,isLoading} = useProductStore();
+
+  useEffect(() => {
+    fetchFeaturedProducts();
+  }, [fetchFeaturedProducts]);
 
 	return (
 		<div className='relative min-h-screen text-white overflow-hidden'>
@@ -29,7 +39,7 @@ const HomePage = () => {
 					))}
 				</div>
 
-		
+      {!isLoading && products?.length > 0 && <FeaturedProducts featuredProducts={products}/>}
 			</div>
 		</div>
 	);
