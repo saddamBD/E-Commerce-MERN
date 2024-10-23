b123e8f609d6883552e24702f2b14c2c976c3128
saddamBD <saddam.ruet@gmail.com>
2024-10-23 20:37:37 +0600

    script define for deplopment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
 import { connectDB } from "./lib/db.js";
@@ -14,6 +15,8 @@ const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resoleve();
+
 app.use(express.json());
 app.use(cookieParser());
 
@@ -23,6 +26,14 @@ app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log("App is listening on http://localhost:" + PORT);
